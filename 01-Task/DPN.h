790f86d36b1d168b458eb5a5872b8e1914d5cc98
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-02-23 23:55:10 +0300

    Create DPN discription

---
+++ 01-Task/DPN.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Divided Positive Number
+class DPN {
+private:
+    unsigned int value;
+    int* divisors;
+    int divisors_size;
+    bool is_sorted;
+
+public:
+    DPN(unsigned int value);
+    DPN(const DPN& other);
+    ~DPN();
+
+    DPN& operator=(const DPN& other);
+    DPN operator+(const DPN& other) const;
+
+    void reinitialize();
+    void change_divisor(unsigned int x, unsigned int y);
+
+    unsigned int gcd(const DPN& other);
+    unsigned int lcm(const DPN& other);
+};
